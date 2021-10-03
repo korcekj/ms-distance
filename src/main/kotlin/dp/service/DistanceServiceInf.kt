b05041df9c60b804de44c8f952d36785cb28f21f
Ján Korček <korcekdevelopment@gmail.com>
2021-10-03 13:06:26 +0200

    refactor: remove global service interface

--- src/main/kotlin/dp/service/DistanceServiceInf.kt
+++
@@ -1,8 +0,0 @@
-package dp.service
-
-import dp.model.Distance
-
-interface DistanceServiceInf {
-    fun createDistance(from: String, to: String): Distance?
-    fun getDistance(from: String, to: String): Distance?
-}
