e36f4d356f4781a6923b1520b6728c3aea41fcdf
Ján Korček <korcekdevelopment@gmail.com>
2021-10-03 15:03:01 +0200

    feat: add ip route tests

--- src/test/kotlin/dp/ApplicationTest.kt
+++ src/test/kotlin/dp/ApplicationTest.kt
@@ -11,28 +11,51 @@ import dp.model.*
 
 class ApplicationTest {
     /**
-     * Verifies that the Distance route returns HTTP 400 status
+     * Verifies that the Distance routes returns HTTP 400 status
      */
     @Test
-    fun testDistanceWithoutParams() = withTestApplication(Application::module) {
+    fun testDistanceRoutesWithoutParams() = withTestApplication(Application::module) {
         with(handleRequest(HttpMethod.Get, "/address")) {
+            assertEquals(HttpStatusCode.BadRequest, response.status())
+            assertEquals("Distance can't be calculated", response.content)
+        }
+        with(handleRequest(HttpMethod.Get, "/ip")) {
             assertEquals(HttpStatusCode.BadRequest, response.status())
             assertEquals("Distance can't be calculated", response.content)
         }
     }
 
     /**
-     * Verifies that the Distance route returns JSON properly formatted and
+     * Verifies that the Address route returns JSON properly formatted and
      * the minimal distance along with the duration will be calculated
      */
     @Test
-    fun testDistanceWithParams() = withTestApplication(Application::module) {
+    fun testAddressWithParams() = withTestApplication(Application::module) {
         with(handleRequest(HttpMethod.Get, "/address?from=Slovakia&to=Russia")) {
             val distance = Distance(
                 from = Place(address = "Slovakia", lat = 48.669026, lng = 19.699024),
                 to = Place(address = "Russia", lat = 61.52401, lng = 105.318756),
                 distance = 5224325,
                 duration = 23509
+            ).json
+
+            assertEquals(HttpStatusCode.OK, response.status())
+            assertEquals(distance, response.content)
+        }
+    }
+
+    /**
+     * Verifies that the Ip route returns JSON properly formatted and
+     * the minimal distance along with the duration will be calculated
+     */
+    @Test
+    fun testIpWithParams() = withTestApplication(Application::module) {
+        with(handleRequest(HttpMethod.Get, "/ip?from=85.198.185.26&to=188.133.153.161")) {
+            val distance = Distance(
+                from = Place(address = "Dnipro", lat = 48.464717, lng = 35.046183),
+                to = Place(address = "Moscow", lat = 55.755826, lng = 37.6173),
+                distance = 830024,
+                duration = 3735
             ).json
 
             assertEquals(HttpStatusCode.OK, response.status())
