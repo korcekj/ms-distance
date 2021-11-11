c543cea2802241bb282f3b8f939443dd86f7d315
Ján Korček <korcekdevelopment@gmail.com>
2021-11-11 10:08:40 +0100

    fix: update test case response

--- src/test/kotlin/dp/ApplicationTest.kt
+++ src/test/kotlin/dp/ApplicationTest.kt
@@ -52,8 +52,8 @@ class ApplicationTest {
     fun testIpWithParams() = withTestApplication(Application::module) {
         with(handleRequest(HttpMethod.Get, "/ip?from=85.198.185.26&to=188.133.153.161")) {
             val distance = Distance(
-                from = Place(address = "Dnipro", lat = 48.464717, lng = 35.046183),
-                to = Place(address = "Moscow", lat = 55.755826, lng = 37.6173),
+                from = Place(address = "Dnipro, UA", lat = 48.464717, lng = 35.046183),
+                to = Place(address = "Moscow, RU", lat = 55.755826, lng = 37.6173),
                 distance = 830024,
                 duration = 3735
             ).json
